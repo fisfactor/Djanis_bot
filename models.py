759fe8fac7969c06d182267786113adaaed444d3
fisfactor <fisfactor@yandex.ru>
2025-05-10 21:00:42 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -2,7 +2,7 @@
 
 import os
 from datetime import datetime
-from sqlalchemy import create_engine, Column, BigInteger, Integer, Boolean, DateTime, String, JSON, func   
+from sqlalchemy import create_engine, Column, BigInteger, Integer, Boolean, DateTime, String, JSON, text   
 from sqlalchemy.orm import declarative_base, sessionmaker
 from dateutil.relativedelta import relativedelta
 
@@ -15,16 +15,59 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users'
-    id           = Column(BigInteger, primary_key=True, autoincrement=True)
-    user_id      = Column(BigInteger, unique=True, nullable=False, index=True)
-    usage_count  = Column(Integer, default=0, nullable=False)
-    is_admin     = Column(Boolean, default=False, nullable=False)
-    first_request  = Column(DateTime,  default=datetime.utcnow, nullable=False)
-    last_request = Column(DateTime, default=datetime.utcnow, nullable=False)
-
-    tariff        = Column(String, default='', nullable=False)     # '', 'БМ','БГ','РМ','РГ'
-    tariff_paid   = Column(Boolean, default=False, nullable=False)
-    advisors      = Column(JSON,   default=list, nullable=False)   # ["Каноны","ЗАГС"]
+    id           = Column(
+        BigInteger,
+        primary_key=True,
+        autoincrement=True
+        )
+    user_id      = Column(
+        BigInteger,
+        unique=True,
+        nullable=False,
+        index=True
+        )
+    usage_count  = Column(
+        Integer,
+        default=0,
+        server_default=text('0'),
+        nullable=False
+        )
+    is_admin     = Column(
+        Boolean,
+        default=False,
+        server_default=text('false'),
+        nullable=False
+        )
+    first_request  = Column(
+        DateTime,
+        default=datetime.utcnow,
+        server_default=text('CURRENT_TIMESTAMP'),
+        nullable=False
+        )
+    last_request = Column(
+        DateTime,
+        default=datetime.utcnow,
+        server_default=text('CURRENT_TIMESTAMP'),
+        nullable=False
+        )
+    tariff        = Column(
+        String,
+        default='',
+        server_default=text("''"),
+        nullable=False
+        )     
+    tariff_paid   = Column(
+        Boolean,
+        default=False,
+        server_default=text('false'),
+        nullable=False
+        )
+    advisors      = Column(
+        JSON,
+        default=list,
+        server_default=text("'[]'"),
+        nullable=False
+        )  
 
     def tariff_expires(self) -> datetime:
         """Дата начала тарифа берётся из first_request, длительность — по типу."""
